e1d3c484cf09cb4e43d65d5f0a8cc25c46a0989b
Vektor <h269368@stud.u-szeged.hu>
2024-05-16 16:41:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advertisements</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <h1>Welcome to the front page!</h1>
+    <p><a>Here</a> you can see all the advertisements that are currently active.</p>
+    <p><a>Here</a> is a list of all our registered users:</p>
+</body>
+</html>
